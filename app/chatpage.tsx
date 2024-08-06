2388664d4febdb1a74e5456bf5f82bda73bee823
JYC0413 <juyichen0413@foxmail.com>
2024-08-06 18:00:07 +0800

    fix

--- app/chatpage.tsx
+++ app/chatpage.tsx
@@ -139,13 +139,13 @@ function ChatPage() {
             <div className="fixed bottom-0 left-0 flex flex-col gap-4 h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
               <a
                 className="pointer-events-none flex place-items-center items-center justify-center gap-2 p-8 lg:pointer-events-auto lg:p-0 font-sans text-lg"
-                href="https://llamaedge.com/"
+                href="https://www.gaianet.ai/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Built by{''}
                 <Image
-                  src={'https://llamaedge.com/src/media/llamaedge-with-black.svg'}
+                  src={'https://www.gaianet.ai/imgs/logo.png'}
                   alt="Supermemory Logo"
                   width={160}
                   height={30}
